7d2e3cb8a26f45e9dc85bb98802f7a4d6ff01b39
Trish Ogen <patriciaogen@gmail.com>
2019-05-10 02:30:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,12 @@
 def turn_count(board)
+  count = 0
   board.each do |position|
-    
+    if |position| == 'X' || |position| == 'O'
+      count += 1
+    end
+  count  
 end
 
-def current_player
-  
+def current_player(board)
+  if turn_count(board) % 2
 end
